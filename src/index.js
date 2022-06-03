41d5d9ff00f039e5723d17031f90a45adb83b1e6
jensenrex <jensenrex@gmail.com>
2022-06-03 13:18:19 +1000

    finish project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 ReactDOM.render(
   <React.StrictMode>
